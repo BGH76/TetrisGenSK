6ee8e565f1c0cca7fa2346fedbea3e3a9e890e28
George <mettin60@yahoo.com>
2022-08-04 18:56:16 -0500

    Rotation of the Blocks in the Box
    The L shaped and the Stick shaped Box are rotating properly in the box. T and Dog shape still being worked on.

--- src/main/java/bgps/tetrisgensk/Controller.java
+++ src/main/java/bgps/tetrisgensk/Controller.java
@@ -18,74 +18,30 @@ public class Controller {
 
     public void rotateBlocks(ArrayList<Block> list, ArrayList<Block>  nonActiveList, KeyCode key) {
 
-        //Todo: refactor using helper methods
-
-        // determine form of shape l
-        if(list.get(0).getX() == list.get(3).getX()){ // if top and bottom block have same x, then the l shape is in vertical form
-            form = 1;
-        }
-        if(list.get(0).getY() == list.get(3).getY()){ // if 0 and 3 block have same Y, then the l shape is in horizontal form
-            form = 2;
-        }
-        if(list.get(0).getX() == list.get(3).getX() && list.get(0).getY()+3 == list.get(3).getY() ){ // l shape is in downward form
-            form = 3;
-        }
-        if (list.get(0).getY() == list.get(3).getY() && list.get(0).getX()-3 == list.get(3).getX()){ //l shape is horizontal pt.2
-            form = 4;
-        }
-
-        // Todo: determine shape of piece
+        form = getBlockOrientation(list);
 
         //rotate accordingly
         switch (key) {
 
             case UP:
-                //l shape
-                if(form == 1) {                             // if piece is vertical
-                    list.get(1).setX(list.get(0).getX()+1); // moves second block from bottom, 1 space to the right of bottom block
-                    list.get(1).setY(list.get(0).getY());
-                    list.get(2).setX(list.get(0).getX()+2);// moves second block from top, 2 spaces to the right of bottom block
-                    list.get(2).setY(list.get(0).getY());
-                    list.get(3).setX(list.get(0).getX()+3); // moves top block 3 spaces to the right of bottom block
-                    list.get(3).setY(list.get(0).getY());
-                }
-                else if(form == 2) {                                      // if piece is horizontal
-                    list.get(1).setY(list.get(0).getY()+1);
-                    list.get(1).setX(list.get(0).getX());
-
-                    list.get(2).setY(list.get(0).getY()+2);
-                    list.get(2).setX(list.get(0).getX());
-
-                    list.get(3).setY(list.get(0).getY()+3);
-                    list.get(3).setX(list.get(0).getX());
-                }
-                else if (form == 3){                                      //if piece is downward
-                    list.get(1).setX(list.get(0).getX()-1);
-                    list.get(1).setY(list.get(0).getY());
-                    list.get(2).setX(list.get(0).getX()-2);
-                    list.get(2).setY(list.get(0).getY());
-                    list.get(3).setX(list.get(0).getX()-3);
-                    list.get(3).setY(list.get(0).getY());
-                }
-                else if(form == 4) {                                      // if piece is horizontal pt. 2
-                    list.get(1).setY(list.get(0).getY()-1);
-                    list.get(1).setX(list.get(0).getX());
-
-                    list.get(2).setY(list.get(0).getY()-2);
-                    list.get(2).setX(list.get(0).getX());
-
-                    list.get(3).setY(list.get(0).getY()-3);
-                    list.get(3).setX(list.get(0).getX());
-                }
-
-                //Todo:
                 //T shape
+                if(list.get(0).getBlockType() == 1)
+                    list = getRotatedTBlock(list,form);
 
-                //Square shape
+                //Square shape No Rotation Needed
+
+                // Stick shape
+                else if(list.get(0).getBlockType() == 3) {
+                    getRotatedStickBlock(list,nonActiveList,form);
+                }
 
                 //L shape
+                else if(list.get(0).getBlockType() == 4)
+                    getRotatedLBlock(list,nonActiveList,form);
 
                 // Z shape
+                else if(list.get(0).getBlockType() == 5)
+                    list = getRotatedDogBlock(list,form);
 
                 break;
 
@@ -162,26 +118,148 @@ public class Controller {
             case 4://L
                 if(list.get(0).getX() == list.get(1).getX() & list.get(0).getX() == list.get(2).getX())//All Xs are same
                 {
-                    if (list.get(0).getX() - list.get(3).getX() < 0)//Is the block pointing up L
-                        orientation = 2;
-                    else
+                    if (list.get(0).getX() - list.get(3).getX() < 0) //Down -> Left
                         orientation = 1;
+                    else//Is the block pointing up L -> right
+                        orientation = 2;
                 }
                 else{//All Ys are same
-                    if (list.get(0).getY() - list.get(3).getY() < 0)//Is the block pointing right
-                        orientation = 3;
-                    else//pointing left
+                    if (list.get(0).getY() - list.get(3).getY() < 0)//Is the block pointing right -> down
                         orientation = 4;
+                    else//pointing left -> up
+                        orientation = 3;
                 }
                 break;
             case 5://Dog
+                if(list.get(0).getX() - list.get(3).getX() == 2)//Horoixontal Z
+                {
+                    if(list.get(0).getY() > list.get(3).getY())
+                        orientation = 4; //left
+                    else
+                        orientation = 3;//right
+
+                }
+                else //Vertical
+                {
+                    if(list.get(0).getX() > list.get(3).getX())
+                        orientation = 1; //Up
+                    else
+                        orientation = 2;//Down
+
+                }
                 break;
         }
         return orientation;
     }
 
-    //Rotate Block
+    //Rotate L Block
+    public void getRotatedLBlock(ArrayList<Block> list, ArrayList<Block>  nonActiveList, int orientation){
 
+        if(orientation == 1){//Down -> Left
+            if(checkForBlocksAndBorder(nonActiveList,list.get(1).getX(),list.get(1).getY(),list.get(0).getX() - 1,list.get(0).getY(),list.get(0).getX() - 2,list.get(0).getY()))
+            {
+                list.get(3).setX(list.get(1).getX());
+                list.get(3).setY(list.get(1).getY());
+                list.get(1).setX(list.get(0).getX() - 1);
+                list.get(1).setY(list.get(0).getY());
+                list.get(2).setX(list.get(0).getX() - 2);
+                list.get(2).setY(list.get(0).getY());
+            }
+        }
+        else if(orientation == 2){//up -> right
+            if(checkForBlocksAndBorder(nonActiveList,list.get(1).getX(),list.get(1).getY(),list.get(0).getX() + 1,list.get(0).getY(),list.get(0).getX() + 2,list.get(0).getY()))
+            {
+                list.get(3).setX(list.get(1).getX());
+                list.get(3).setY(list.get(1).getY());
+                list.get(1).setX(list.get(0).getX() + 1);
+                list.get(1).setY(list.get(0).getY());
+                list.get(2).setX(list.get(0).getX() + 2);
+                list.get(2).setY(list.get(0).getY());
+            }
+        }
+        else if(orientation == 3){//right -> down
+            if(checkForBlocksAndBorder(nonActiveList,list.get(1).getX(),list.get(1).getY(),list.get(0).getX(),list.get(0).getY() + 1,list.get(0).getX(),list.get(0).getY() + 2))
+            {
+                list.get(3).setX(list.get(1).getX());
+                list.get(3).setY(list.get(1).getY());
+                list.get(1).setX(list.get(0).getX());
+                list.get(1).setY(list.get(0).getY() + 1);
+                list.get(2).setX(list.get(0).getX());
+                list.get(2).setY(list.get(0).getY() + 2);
+            }
+
+        }
+        else{//left -> up
+            if(checkForBlocksAndBorder(nonActiveList,list.get(1).getX(),list.get(1).getY(),list.get(0).getX(),list.get(0).getY() - 1,list.get(0).getX(),list.get(0).getY() - 2))
+            {
+                list.get(3).setX(list.get(1).getX());
+                list.get(3).setY(list.get(1).getY());
+                list.get(1).setX(list.get(0).getX());
+                list.get(1).setY(list.get(0).getY() - 1);
+                list.get(2).setX(list.get(0).getX());
+                list.get(2).setY(list.get(0).getY() - 2);
+            }
+        }
+
+    }
+
+    //Rotate the
+
+    //Rotate Stick Block
+    public void getRotatedStickBlock(ArrayList<Block> list, ArrayList<Block>  nonActiveList, int orientation){
+
+        if(orientation == 1)//Vertical
+        {//Turn horizontal
+            if(checkForBlocksAndBorder(nonActiveList,list.get(0).getX()+1, list.get(0).getY(),list.get(0).getX()+2,list.get(0).getY(),list.get(0).getX()+3,list.get(0).getY()))
+            {
+                list.get(1).setX(list.get(0).getX() + 1); // moves second block from bottom, 1 space to the right of bottom block
+                list.get(1).setY(list.get(0).getY());
+                list.get(2).setX(list.get(0).getX() + 2);// moves second block from top, 2 spaces to the right of bottom block
+                list.get(2).setY(list.get(0).getY());
+                list.get(3).setX(list.get(0).getX() + 3); // moves top block 3 spaces to the right of bottom block
+                list.get(3).setY(list.get(0).getY());
+            }
+            //On rotation it should not hit the sides
+        }
+        else {//Horizontal turn vertical
+            if(checkForBlocksAndBorder(nonActiveList,list.get(3).getX(),list.get(3).getY() + 1,list.get(3).getX(),list.get(3).getY() + 2,list.get(3).getX(),list.get(3).getY() + 3))
+            {
+                list.get(0).setX(list.get(3).getX()); // moves second block from bottom, 1 space to the right of bottom block
+                list.get(0).setY(list.get(3).getY() + 1);
+                list.get(1).setX(list.get(3).getX());// moves second block from top, 2 spaces to the right of bottom block
+                list.get(1).setY(list.get(3).getY() + 2);
+                list.get(2).setX(list.get(3).getX()); // moves top block 3 spaces to the right of bottom block
+                list.get(2).setY(list.get(3).getY() + 3);
+            }
+        }
+    }
+
+    //Rotate T Block
+    public ArrayList<Block> getRotatedTBlock(ArrayList<Block> list, int orientation){
+        return list;
+    }
+
+    //Rotate Dog Block
+    public ArrayList<Block> getRotatedDogBlock(ArrayList<Block> list, int orientation){
+        return list;
+    }
+
+    public boolean checkForBlocksAndBorder(ArrayList<Block> list, int  ...pos){
+        {
+            for(int i = 0; i < pos.length; i += 2) {
+                int x = pos[i], y = pos[i+1];
+                List<Block> temp = list.stream()
+                        .filter(f -> f.getY() == y && f.getX() == x)
+                        .collect(Collectors.toList());
+                if(temp.size()!=0)
+                    return false;
+                if(x < 0 | x > 8 | y < 0 |y > 19)
+                    return false;
+            }
+            return true;
+        }
+
+    }
     //Todo: check if move is valid given the constraints
     public boolean checkMove(ArrayList<Block>list) {
 
